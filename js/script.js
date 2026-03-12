c3b482e3540773ea1c0ed1893e94fa9f35486372
Sujan Kumar Roy <sujanroy63836@gmail.com>
2026-03-12 20:14:16 +0530

    removed unused css and js;

--- js/script.js
+++ js/script.js
@@ -80,55 +80,7 @@ function saveHistory(area, unit, amount) {
     localStorage.setItem("history", JSON.stringify(history));
 }
 
-function showHistory() {
-    historySection.classList.remove("hidden");
-    let history = JSON.parse(localStorage.getItem("history")) || [];
-    historyList.innerHTML = "";
-    history.forEach(h => {
-        historyList.innerHTML += `
-            <div>
-                <strong>${h.date}</strong><br>
-                Area: ${h.area}<br>
-                Amount: ${h.amount}<br><br>
-            </div>
-        `;
-    });
-}
-
-function clearHistory() {
-    localStorage.removeItem("history");
-    historyList.innerHTML = "No History";
-}
-
 function clearAll() {
     document.querySelectorAll("input").forEach(i => i.value = "");
     result.innerHTML = "";
-}
-
-function exportHistory() {
-    let history = JSON.parse(localStorage.getItem("history")) || [];
-
-    if(history.length === 0){
-        alert("No history to export.");
-        return;
-    }
-
-    let content = "Land Calculator History\n\n";
-
-    history.forEach(h => {
-        content += `Date: ${h.date}\n`;
-        content += `Area: ${h.area}\n`;
-        content += `Amount: ${h.amount}\n`;
-        content += "--------------------------\n";
-    });
-
-    let blob = new Blob([content], { type: "text/plain" });
-    let url = URL.createObjectURL(blob);
-
-    let a = document.createElement("a");
-    a.href = url;
-    a.download = "Land_History.txt";
-    a.click();
-
-    URL.revokeObjectURL(url);
 }
